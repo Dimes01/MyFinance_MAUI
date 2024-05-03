3a58aafb926b3a20eebee2f3971bd67e9faf4567
Dimes01 <tyapin2002@mail.ru>
2024-05-03 21:12:14 +0400

    добавлены viewmodels

---
+++ MyFinance/ViewModels/IncomesPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFinance.ViewModels;
+
+class IncomesPageViewModel
+{
+
+	propchanged
+
+
+
+	public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
