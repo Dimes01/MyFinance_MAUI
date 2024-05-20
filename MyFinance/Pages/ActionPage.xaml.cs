9ff117cd2a777e6e47772aaedc895f34e1d9d87f
Дмитрий Тяпин <tyapin2002@mail.ru>
2024-05-20 21:14:24 +0400

    фикс вьюмоделей

--- MyFinance/Pages/ActionPage.xaml.cs
+++ MyFinance/Pages/ActionPage.xaml.cs
@@ -4,22 +4,43 @@ namespace MyFinance.Pages;
 
 public partial class ActionPage : ContentPage
 {
+    private bool isAdd = false;
+    public Transaction Transaction;
+
 	public ActionPage(Transaction transaction = null)
 	{
 		InitializeComponent();
-		Init(transaction);
+        if (transaction == null)
+        {
+            isAdd = true;
+            Transaction = new Transaction();
+        }
+        else Transaction = transaction;
+		Init();
 	}
 
-	private void Init(Transaction transaction)
+	private void Init()
 	{
-		if (transaction is not null)
+		if (isAdd == false)
 		{
-			IdProperty.ValueProperty = transaction.Id.ToString();
-            TypeProperty.ValueProperty = transaction.Type.ToString();
-            CostProperty.ValueProperty = transaction.Cost.ToString();
-            CategoryProperty.ValueProperty = transaction.Category.ToString();
+			IdProperty.ValueProperty = Transaction.Id.ToString();
+            TypeProperty.ValueProperty = Transaction.Type.ToString();
+            CostProperty.ValueProperty = Transaction.Cost.ToString();
+            CategoryProperty.ValueProperty = Transaction.Category.ToString();
         }
     }
 
-	
+    private async void ButtonSafe_Clicked(object sender, EventArgs e)
+    {
+        Transaction.Id = Convert.ToInt64(IdProperty.ValueProperty);
+        Transaction.Type = TypeProperty.ValueProperty;
+        Transaction.Cost = Convert.ToDouble(CostProperty.ValueProperty);
+        Transaction.Category = CategoryProperty.ValueProperty;
+        await Navigation.PopAsync();
+    }
+
+    private async void ButtonCancel_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
 }
