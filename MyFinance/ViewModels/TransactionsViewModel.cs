66a171a607c431b08136576e0b515b08940f2487
Дмитрий Тяпин <tyapin2002@mail.ru>
2024-05-29 18:46:23 +0400

    добавлены стартовые данные

--- MyFinance/ViewModels/TransactionsViewModel.cs
+++ MyFinance/ViewModels/TransactionsViewModel.cs
@@ -18,7 +18,11 @@ class TransactionsViewModel : INotifyPropertyChanged
 	private int PositionSelectedTransaction {  get; set; }
 
 
-    private ObservableCollection<Transaction> transactions = [];
+	private ObservableCollection<Transaction> transactions = [
+		new Transaction { Id = 1, Type = "Доход", Cost = 6500, Category = "Зарплата", Date = DateTime.Parse("05.06.2024 12:05:42") },
+		new Transaction { Id = 2, Type = "Доход", Cost = 5000, Category = "Перевод", Date = DateTime.Parse("04.06.2024 10:05:22") },
+		new Transaction { Id = 3, Type = "Расход", Cost = 3000, Category = "Покупка", Date = DateTime.Parse("06.06.2024 11:05:42") }
+	];
 	public ObservableCollection<Transaction> Transactions
 	{
 		get => transactions;
@@ -31,7 +35,7 @@ class TransactionsViewModel : INotifyPropertyChanged
 	}
 
 
-	private HashSet<string> categories;
+	private HashSet<string> categories = ["Зарплата", "Перевод", "Покупка"];
 	public HashSet<string> Categories
 	{
 		get => categories;
