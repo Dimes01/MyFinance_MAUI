816f972577aa84876ad37ee98d8efe71efecf491
Dimes01 <tyapin2002@mail.ru>
2024-06-09 22:44:06 +0400

    рефакторинг mauiprogram

--- MyFinance/MauiProgram.cs
+++ MyFinance/MauiProgram.cs
@@ -3,30 +3,54 @@ using MyFinance.Pages;
 using MyFinance.Services;
 using MyFinance.ViewModels;
 
-namespace MyFinance
+namespace MyFinance;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+		builder.Logging.AddDebug();
 #endif
 
-            builder.Services.AddTransient<AuthService>();
-            builder.Services.AddSingleton<ShareData>();
-            builder.Services.AddSingleton<MainPageViewModel>();
+        builder.RegisterPages();
+        builder.RegisterViewModels();
+        builder.RegisterOtherServices();
 
-            return builder.Build();
-        }
+        return builder.Build();
+    }
+
+    private static MauiAppBuilder RegisterPages(this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.AddSingleton<LoginPage>();
+        mauiAppBuilder.Services.AddSingleton<MainPage>();
+        mauiAppBuilder.Services.AddSingleton<IncomesPage>();
+        mauiAppBuilder.Services.AddSingleton<ExpendituresPage>();
+        mauiAppBuilder.Services.AddTransient<ActionPage>();
+        return mauiAppBuilder;
+    }
+
+    private static MauiAppBuilder RegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.AddSingleton<LoginPageViewModel>();
+        mauiAppBuilder.Services.AddSingleton<MainPageViewModel>();
+        mauiAppBuilder.Services.AddTransient<TransactionsViewModel>();
+        return mauiAppBuilder;
+    }
+
+    private static MauiAppBuilder RegisterOtherServices(this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.AddSingleton<AuthService>();
+        mauiAppBuilder.Services.AddSingleton<ShareData>();
+        return mauiAppBuilder;
     }
 }
